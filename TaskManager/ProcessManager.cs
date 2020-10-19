8e54ada31083948a040b1a41c79e9d7bf2395a32
AlexanderLukashuk <upiter465@gmail.com>
2020-10-20 01:44:47 +0600

    Добавлен DataGrid

---
+++ TaskManager/ProcessManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace TaskManager
+{
+    class ProcessManager
+    {
+        public Process[] processes = Process.GetProcesses();
+    }
+}
